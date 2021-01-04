31c404de539be48c0c8b2415a822c784d7b1a32f
AshikuJr <ashikujr@gmail.com>
2021-01-05 04:05:05 +0700

    navbar

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import '../styles.sass';
 
+import Navbar from './navbar/navbar.jsx';
+
 const App = () => {
-  return <div className="App">hello world</div>
+  return <div className="app">
+    <div className="container"><Navbar /></div>
+  
+  </div>
 };
 
 export default App;
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './navbar.sass';
+import logo from './logo.svg';
+
+const Navbar__link = props => {
+  return <a href={props.src} className="navbar__link">{props.text}</a>
+}
+
+const Navbar = props => {
+  return <header className="navbar">
+    <img src={logo} alt="logo" className="navbar__logo"/>
+    <a href="#" className="navbar__brand">Lasles <span style={{fontFamily: 'Bold'}}>VPN</span></a>
+    <Navbar__link text="About" src="#"/>
+    <Navbar__link text="Features" src="#"/>
+    <Navbar__link text="Pricing" src="#"/>
+    <Navbar__link text="Testimonials" src="#"/>
+    <Navbar__link text="Help" src="#"/>
+    <a href="#" className="navbar__signIn">Sign In</a>
+    <button className="navbar__signUp">Sign Up</button>
+  </header>
+};
+
+export default Navbar;
